95ff551f7e1e01447386b345d0dcd02d7a5ebb06
Shourav Sarker <ssarker152167@bscse.uiu.ac.bd>
2021-08-04 12:48:46 +0600

    intial files

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+inr main(){
+
+    return 0;
+}
+int add(int x.int y){
+
+    return x+y;
+
+}
+int sub(int x,int y){
+    return x-y;
+}
+
+int multiply(int x,int y){
+
+    return x*y;
+}
